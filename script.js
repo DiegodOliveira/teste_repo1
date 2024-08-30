ca83ab0156dd46bbcd18df4699d1bc055d30c414
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2024-08-30 19:27:21 -0300

    commit

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ const messages = [
     "A cada dia nos amamos mais",
     "Não importa o quanto eu cite suas qualidades, pois você é indescritível",
     "Tão doce, querida, meiga e gentil",
-    "Feliz 4 meses meu bem",
-    "https://youtu.be/Sz7LQUHRxTI copie e veja no youtube"
+    "Feliz 4 meses meu bem"
 ];
 
 // Index da mensagem atual
 let currentIndex = -1; // Começa em -1 para que o primeiro clique mostre a primeira mensagem
 
-// Função para trocar a mensagem
+// Função para trocar a mensagem ou redirecionar
 function changeMessage() {
-    // Alterna entre as mensagens
-    currentIndex = (currentIndex + 1) % messages.length;
-    // Atualiza o texto no div
-    document.getElementById('div-texto').textContent = messages[currentIndex];
+    // Incrementa o índice
+    currentIndex += 1;
+
+    // Verifica se o índice ultrapassou o último índice do array
+    if (currentIndex < messages.length) {
+        // Atualiza o texto no div
+        document.getElementById('div-texto').textContent = messages[currentIndex];
+    } else {
+        // Redireciona para outra página após a última mensagem
+        window.location.href = "SOldieroflove.html"; // Substitua "pagina2.html" pela URL desejada
+    }
 }
 
 // Adiciona o evento de clique ao botão
